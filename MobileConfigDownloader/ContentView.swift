d7dfb1ad59f63c00a2ef325ba9047e9541cc32fb
Taehyen Kang <appletaehyen@gmail.com>
2024-02-01 16:32:45 +0900

    init

---
+++ MobileConfigDownloader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MobileConfigDownloader
+//
+//  Created by 3i-A1-2022-033 on 2/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MobileConfigDownloader/MobileConfigDownloaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileConfigDownloaderApp.swift
+//  MobileConfigDownloader
+//
+//  Created by 3i-A1-2022-033 on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct MobileConfigDownloaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
